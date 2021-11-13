fe276de93581f85fa0114788b7fb5a863b664723
jhon-patiu <patiunohj@gmail.com>
2021-11-13 21:41:56 +0800

    add meta tags to feed component

--- pages/eom.js
+++ pages/eom.js
@@ -3,18 +3,20 @@ import * as styles from "../styles/eom.module.css";
 
 const EOM = ({ employee }) => {
     return (
-        <div className="page-container">
-            <Toolbar />
-            <div className={styles.main}>
-                <h1>Employee of the Month</h1>
-                <div className={styles.eom}>
-                    <h2>{employee.name}</h2>
-                    <h4>{employee.position}</h4>
-                    <img src={employee.image} alt=" " />
-                    <p>{employee.description}</p>
+        <>
+            <div className="page-container">
+                <Toolbar />
+                <div className={styles.main}>
+                    <h1>Employee of the Month</h1>
+                    <div className={styles.eom}>
+                        <h2>{employee.name}</h2>
+                        <h4>{employee.position}</h4>
+                        <img src={employee.image} alt=" " />
+                        <p>{employee.description}</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
