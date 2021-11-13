f03afb478f7ab4af3b82f30bf5c1af892bc98583
jhon-patiu <patiunohj@gmail.com>
2021-11-13 21:18:02 +0800

    add target blank to fb link and rel noopener

--- components/Toolbar.js
+++ components/Toolbar.js
@@ -2,12 +2,17 @@ import Link from "next/link";
 import styles from "../styles/Toolbar.module.css";
 
 export const Toolbar = () => {
-  return (
-    <div className={styles.main}>
-      <Link href="/">Home</Link>
-      <Link href="/feed/1">Feed</Link>
-      <Link href="/eom">Staff</Link>
-      <Link href="https://www.facebook.com/PatiuJhon">Facebook</Link>
-    </div>
-  );
+    return (
+        <div className={styles.main}>
+            <Link href="/">Home</Link>
+            <Link href="/feed/1">Feed</Link>
+            <Link href="/eom">Staff</Link>
+            <a
+                href="https://www.facebook.com/PatiuJhon"
+                target="_blank"
+                rel="noreferrer noopener">
+                Facebook
+            </a>
+        </div>
+    );
 };
